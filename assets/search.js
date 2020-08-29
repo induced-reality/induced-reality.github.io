3cae71d9b972fc05de6eefa3b64fba4603aebfd7
Abdeselam El-Haman <aelhaman@gmail.com>
2020-08-29 15:26:27 +0200

    static site

--- assets/search.js
+++ assets/search.js
@@ -1,3 +1,1 @@
----
----
-{%- include search-providers/default/search-data.js -%}
+window.TEXT_SEARCH_DATA={'posts':[{'title':"Post with Header Image",'url':"/2018/06/01/header-image.html"},{'title':"Welcome",'url':"/2018/07/01/welcome.html"}]};
